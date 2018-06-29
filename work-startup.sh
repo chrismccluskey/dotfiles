921871b754a2ceba9c76f56a9852df0ea6794144
Chris McCluskey <cmccluskey@digitaltrends.com>
2018-06-29 08:55:46 -0700

    Changes from i5 mac at DT

--- work-startup.sh
+++ work-startup.sh
@@ -1,6 +1,8 @@
-open -a Alfred\ 3
+#open -a Alfred\ 3
 open -a Divvy
 # open -a TogglDesktop
 open -a Todoist
-open -a FirefoxDeveloperEdition
-open -a VimR
+open -a Tomato\ One
+open -a Visual\ Studio\ Code
+#open -a VimR
+cd ~/developer; docker-compose up -d
